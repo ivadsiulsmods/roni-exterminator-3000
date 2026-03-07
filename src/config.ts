a191da0c6a85838b869e77292dc2c6db390a9b34
ivadsiuls <ivadsiuls@proton.me>
2026-03-07 16:37:46 -0300

    add mystery man

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const config = {
   watchedUserIds: [
     724142242708717589n, // roni
+    695018629292490813n, // mystery man
     // 1413923760121315409n, // alt acc (debug)
   ] as unknown as bigint[],
 
